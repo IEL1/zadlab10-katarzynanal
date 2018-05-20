c42b69fa14b1eaca0b6af4607c4937eb7847d6b3
darma <darma@DESKTOP-NUMHLGI>
2018-05-20 19:30:08 +0200

    lab10

--- OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
@@ -1,13 +1,31 @@
 package pl.edu.ur.oopl10;
 
-/**
- *
- * @author mchla
- */
-public class Main {
+//author Kasia
 
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main extends java.lang.Exception{
+    
+    
+
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int dzielna = scanner.nextInt();
+        int dzielnik = scanner.nextInt();
+        
+       
+        try {
+            
+            int wynik = dzielna / dzielnik;
+            System.out.println("Niemożliwe !!!");
+        } 
+        catch (ArithmeticException e) {
+            System.out.print("Niepoprawna dzielna");
+        
+            
+        }
         
     }
+    }
     
-}
